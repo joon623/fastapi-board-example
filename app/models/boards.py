3e1f240bc6369b1d051a1912e314b011ac3d2102
jun <nnamjun94@gmail.com>
2023-02-23 10:45:40 +0900

    add docker and docker compose file

--- app/models/boards.py
+++ app/models/boards.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Integer, Column, DateTime, ForeignKeyConstraint, String
+from sqlalchemy import Table, Integer, Column, DateTime, ForeignKeyConstraint, String, ForeignKey
 
 from app.config.connection import metadata
 
@@ -8,11 +8,8 @@ boards = Table(
     Column('id', Integer, primary_key=True, index=True),
     Column("created_at",
            DateTime(timezone=True), nullable=False),
-    Column("username", String(50), nullable=False),
-    Column("email", String(50), nullable=False),
+    Column("username", ForeignKey("users.username"), nullable=False),
+    Column("email", ForeignKey("users.email"), nullable=False),
     Column("title", String(500), nullable=False),
     Column("body", String(2000), nullable=False),
-    ForeignKeyConstraint(
-        ['email', "username"], ["users.email", "users.username"]
-    )
 )
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import DateTime, String, Integer, Table, Column
+from sqlalchemy import DateTime, String, Integer, Table, Column, ForeignKeyConstraint
 from sqlalchemy.dialects.mysql import LONGTEXT
 
 from app.config.connection import metadata
@@ -6,9 +6,9 @@ from app.config.connection import metadata
 users = Table(
     "users",
     metadata,
-    Column('id', Integer, primary_key=True, index=True),
-    Column("email", String(50), unique=True, index=True,  nullable=False),
-    Column("username", String(50), unique=True,  nullable=False),
+    Column('id', Integer, primary_key=True),
+    Column("email", String(50), unique=True, nullable=False),
+    Column("username", String(50), unique=True, nullable=False),
     Column("password", LONGTEXT),
     Column("created_at", DateTime(timezone=True))
 )
